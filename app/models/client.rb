6e33b05018e7380e00c3ee33b3749139eff280b5
Mikael Eriksson <me222wm@student.lnu.se>
2016-04-02 23:49:09 +0200

    Validation to client.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,10 @@
 class Client < ActiveRecord::Base
   belongs_to :user, required: true
-
-  # TODO add slug.
+  has_many :cases, dependent: :destroy
+  validates :first_name, presence: true, length: { maximum: 40 }
+  validates :last_name, presence: true, length: { maximum: 60 }
+  validates :ssn, presence: true,
+                  length: { maximum: 10 },
+                  numericality: { only_integer: true }
+  # TODO: add slug.
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,16 +1,36 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Client, type: :model do
-  let(:client) 
-  client = build(:client)
+  let(:client) do
+    build(:client)
+  end
 
   it "should be valid" do
-    byebug
     expect(client).to be_valid
   end
 
-  it "should not be valid witout first_name" do
+  it "should not be valid without first name" do
     client.first_name = ""
+    expect(client).not_to be_valid
+  end
+
+  it "should not be valid without last name" do
+    client.last_name = ""
+    expect(client).not_to be_valid
+  end
+
+  it "should not be valid without ssn" do
+    client.ssn = ""
+    expect(client).not_to be_valid
+  end
+
+  it "ssn should be numerical" do
+    client.ssn = "A810158B72"
+    expect(client).not_to be_valid
+  end
+
+  it "should not be valid without user" do
+    client.user = nil
     expect(client).not_to be_valid
   end
 end
