cfd2e5a27801e4f42c9099049f8d1e4182ec499b
Mikael Eriksson <me222wm@student.lnu.se>
2016-04-27 09:27:14 +0200

    Small change in app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
  // This is a manifest file that'll be compiled into application.js, which will
 // include all the files listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts,
+// vendor/assets/javascripts, or any plugin's vendor/assets/javascripts directory
+// can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
@@ -14,7 +15,7 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
-//= require es5-shim
+//# require es5-shim
 //= require react
 //= require react_ujs
 //= require components
