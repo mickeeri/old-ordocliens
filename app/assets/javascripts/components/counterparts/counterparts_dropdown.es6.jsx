a2ff1a8fb586e6cf973e41fc0b9a955784166b5f
Mikael Eriksson <me222wm@student.lnu.se>
2016-05-09 22:08:05 +0200

    Dropdown for counterpart.

---
+++ app/assets/javascripts/components/counterparts/counterparts_dropdown.es6.jsx
@@ -0,0 +1,78 @@
+class CounterpartsDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counterparts: [],
+      selectedCounterpart: '',
+    };
+    this.fetchCounterparts = this.fetchCounterparts.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCounterparts();
+  }
+
+  handleSelectChange(e) {
+    this.setState({ selectedCounterpart: e.target.value });
+  }
+
+  handleOnSubmit(e) {
+    e.preventDefault();
+    makePutRequest(Routes.counterpart_path(this.state.selectedCounterpart),
+      { counterpart: { lawsuit_id: this.props.lawsuitId } }, 'counterpartListUpdated');
+  }
+
+  dismissBtnClicked() {
+    PubSub.publish('dismissEdit');
+  }
+
+  fetchCounterparts() {
+    const url = Routes.counterparts_path();
+    makeGetRequest(url)
+      .success(response => {
+        this.setState({ counterparts: response.counterparts });
+      })
+      .error(xhr => {
+        console.error(url, xhr.status, xhr.statusText);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleOnSubmit}>
+          <label htmlFor="counterparts"></label>
+          <div className="form-group">
+            <select
+              className="form-control"
+              onChange={this.handleSelectChange}
+              name="counterparts"
+              value={this.state.selectedCounterpart}
+              required
+            >
+              <option value="" disabled>Välj en motpart</option>
+              {this.state.counterparts.map(counterpart =>
+                <option
+                  key={counterpart.id}
+                  value={counterpart.id}
+                >{counterpart.name} ({counterpart.personalNumber})</option>
+              )}
+            </select>
+          </div>
+          <div className="content-right">
+            <button className="btn btn-secondary btn-sm" onClick={this.dismissBtnClicked}>
+              Avbryt
+            </button>
+            <button className="btn btn-success btn-sm" type="submit">Spara</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+CounterpartsDropdown.propTypes = {
+  lawsuitId: React.PropTypes.number.isRequired,
+};
