0cc6922fc92dd316b348705903677ed0cc0ba902
me222wm <me222wm@student.lnu.se>
2016-05-22 19:54:58 +0200

    Change in eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     ],
     "rules": {
       "no-undef": "off",
-      "react/jsx-no-undef": "off"
+      "react/jsx-no-undef": "off",
+      "no-unused-vars": "off"
     },
     "globals": {
       "React": true
