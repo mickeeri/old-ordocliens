196d2d62a565e97b62dfdfafe450679dddf26499
me222wm <me222wm@student.lnu.se>
2016-05-23 11:59:05 +0200

    Fixed tests.

--- spec/models/lawsuit_type_spec.rb
+++ spec/models/lawsuit_type_spec.rb
@@ -1,5 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe LawsuitType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :lawsuits }
+  it { should validate_presence_of :name }
 end
