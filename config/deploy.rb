083c136e3a7ff056c40d318ffac544872a1a449e
me222wm <me222wm@student.lnu.se>
2016-05-24 14:23:40 +0200

    Deployment config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,8 +64,8 @@ namespace :deploy do
     on primary fetch(:migration_role) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute :rake, "db:reset"
-          #execute :rake, "db:seed"
+          # execute :rake, "db:reset"
+          execute :rake, "db:seed"
         end
       end
     end
