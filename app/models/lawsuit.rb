41ccbc294753369073b29d55366f21c8f69ef2e7
me222wm <me222wm@student.lnu.se>
2016-05-28 15:44:50 +0200

    Clean up lawsuit class.

--- app/models/lawsuit.rb
+++ app/models/lawsuit.rb
@@ -10,29 +10,19 @@ class Lawsuit < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :expenses, dependent: :destroy
   belongs_to :lawsuit_type
-  #has_one :primary_participation, -> { where(is_primary: true) }, class_name: "Participation"
+
+  # Validation
   validates :lawsuit_type, presence: :true
   validates :court, allow_blank: true, length: { maximum: 100 }
   validates :case_number, allow_blank: true, length: { maximum: 20 }
 
-  # def primary_client
-  #   if primary_participation
-  #     name = primary_participation.client.full_name
-  #     name += " m.fl." if clients.count > 1
-  #     pc = { name: name, id: primary_participation.client.id }
-  #   else
-  #     pc = { name: "", id: nil }
-  #   end
-  #   pc
-  # end
-  # TODO: Remove unused scopes.
   # Scopes
-  scope :sorted, -> { includes(:lawsuit_type).order("lawsuit_types.name asc") }
   # https://github.com/bbatsov/rubocop/issues/1520
-  scope :sorted_by_participation, -> { includes(:participations).order("participations.created_at")}
-  scope :sorted_by_client, ->() do
-    includes(:clients).order("clients.last_name asc").order("clients.first_name asc")
-  end
+  scope :sorted_by_clients, -> { includes(:clients)
+    .order("clients.last_name asc")
+    .order("clients.first_name asc")
+    .references(:clients)}
+
   scope :sorted_by_primary_client, -> { includes(:primary_client)
     .order("clients.last_name")
     .order("clients.first_name") }
@@ -44,7 +34,7 @@ class Lawsuit < ActiveRecord::Base
                             :case_number,
                             :slug],
                   associated_against: { lawsuit_type: [:name],
-                                        primary_client: [:last_name,
+                                        clients: [:last_name,
                                                   :first_name,
                                                   :ssn] },
                   using: { tsearch: { prefix: true,
