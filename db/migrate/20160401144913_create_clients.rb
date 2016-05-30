bba4da8ace86133cf182efacce1421a63436e032
me222wm <me222wm@student.lnu.se>
2016-05-30 14:43:38 +0200

    Dont't want to rename migration.

--- db/migrate/20160401144913_create_clients.rb
+++ db/migrate/20160401144913_create_clients.rb
@@ -3,7 +3,7 @@ class CreateClients < ActiveRecord::Migration
     create_table :clients do |t|
       t.string :last_name, index: true
       t.string :first_name, index: true
-      t.string :personal_number, index: true, unique: true
+      t.string :ssn, index: true, unique: true
       t.string :street
       t.string :post_code
       t.string :city
