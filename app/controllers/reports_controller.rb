0128d9b5ba685c28896c3864300e95e2e6fa980d
me222wm <me222wm@student.lnu.se>
2016-06-05 20:58:40 +0200

    Remove cover from reports.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,12 +7,4 @@ class ReportsController < ApplicationController
     @price_categories = PriceCategory.all.sorted
     @expenses = @lawsuit.expenses.sorted
   end
-
-  def cover
-    @lawsuit = Lawsuit.find(params[:id])
-    headers["Content-Disposition"] = "attachment; filename=\"Aktomslag-#{@lawsuit.slug}.docx\""
-    @tasks = @lawsuit.tasks.sorted_by_date
-    @price_categories = PriceCategory.all.sorted
-    @expenses = @lawsuit.expenses.sorted
-  end
 end
