0ed7dc26a435bec5878b1718c2b7187938edbcf4
me222wm <me222wm@student.lnu.se>
2016-06-06 10:08:20 +0200

    Report security.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,8 @@
 class ReportsController < ApplicationController
+  before_action :authenticate_user!
   respond_to :json, :html, :docx
   def show
-    @lawsuit = Lawsuit.find(params[:id])
+    @lawsuit = Lawsuit.within_firm(current_user).find(params[:id])
     headers["Content-Disposition"] = "attachment; filename=\"Rapport-#{@lawsuit.slug}.docx\""
     @tasks = @lawsuit.tasks.sorted_by_date
     @price_categories = PriceCategory.all.sorted
