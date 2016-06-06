0013bc42263d66f00ff163fb9770d7e79f1796e8
me222wm <me222wm@student.lnu.se>
2016-06-07 00:09:54 +0200

    Moving mail settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
         :address              => "smtp.gmail.com",
         :port                 => 587,
         :domain               => "ordocliens.se",
-        :user_name            => ENV["GMAIL_USERNAME"],
-        :password             => ENV["GMAIL_PASSWORD"],
+        :user_name            => Rails.application.secrets.gmail_username,
+        :password             => Rails.application.secrets.gmail_password,
         :authentication       => :plain,
         :enable_starttls_auto => true
   }
