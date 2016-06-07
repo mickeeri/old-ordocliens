d2e789a35fad1ecda8a8997d8a0a54738b99973e
me222wm <me222wm@student.lnu.se>
2016-06-07 13:39:31 +0200

    Firm validation.

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -1,5 +1,7 @@
 class Firm < ActiveRecord::Base
   has_many :counterparts, dependent: :destroy
   has_many :users, dependent: :destroy
-  validates :name, presence: true, length: { maximum: 40 }
+  validates :name, presence: true,
+                   length: { maximum: 40 },
+                   uniqueness: { case_sensitive: false }
 end
