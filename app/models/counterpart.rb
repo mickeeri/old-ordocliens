691a3687c2980f0b785054854a3a0a00ad48dbbd
me222wm <me222wm@student.lnu.se>
2016-06-21 13:11:59 +0200

    Counterpart validation tests.

--- app/models/counterpart.rb
+++ app/models/counterpart.rb
@@ -1,10 +1,18 @@
 class Counterpart < ActiveRecord::Base
+  # t.integer  "firm_id"
+  # t.string   "first_name"
+  # t.string   "last_name"
+  # t.string   "personal_number"
+  # t.string   "representative"
+  # t.text     "info"
+
   include PgSearch
   belongs_to :firm, required: true
   has_many :clients, -> { distinct }, through: :disputes
   has_many :disputes, dependent: :destroy
   has_many :involvements, dependent: :destroy
   has_many :lawsuits, -> { distinct }, through: :involvements
+
   # Validation
   validates :first_name, presence: true, length: { maximum: 60 }
   validates :last_name, presence: true, length: { maximum: 60 }
@@ -12,6 +20,7 @@ class Counterpart < ActiveRecord::Base
   validates :personal_number, presence: true, format: { with: VALID_SSN_REGEX }
   validates :representative, allow_blank: true, length: { maximum: 60 }
   validates :info, allow_blank: true, length: { maximum: 1000 }
+
   # Scopes
   scope :sorted, -> { order(last_name: :asc, first_name: :asc) }
   scope :within_firm, -> (firm) { where(firm_id: firm.id) }
--- spec/models/counterpart_spec.rb
+++ spec/models/counterpart_spec.rb
@@ -1,14 +1,44 @@
 require "rails_helper"
 
 RSpec.describe Counterpart, type: :model do
+  let(:counterpart) { create(:counterpart, firm: create(:firm)) }
+
+  # Presence
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :personal_number }
-  it { should have_db_index(:personal_number) }
+
+  # DB index
   it { should have_db_index(:first_name) }
   it { should have_db_index(:last_name) }
-  it { should have_many :lawsuits }
+  it { should have_db_index(:personal_number) }
+
+  # Relations
   it { should have_many :clients }
-  it { should have_many :involvements }
   it { should have_many :disputes }
+  it { should have_many :involvements }
+  it { should have_many :lawsuits }
+
+  # Length
+  it { should validate_length_of(:first_name).is_at_most(60) }
+  it { should validate_length_of(:info).is_at_most(1000) }
+  it { should validate_length_of(:last_name).is_at_most(60) }
+  it { should validate_length_of(:representative).is_at_most(60) }
+
+  it "should validate personal number format" do
+    counterpart.personal_number = "881015-8272"
+    expect(counterpart).to be_valid
+
+    counterpart.personal_number = "001015-8272"
+    expect(counterpart).to be_valid
+
+    counterpart.personal_number = "8810158272"
+    expect(counterpart).to_not be_valid
+
+    counterpart.personal_number = "88105-82727"
+    expect(counterpart).to_not be_valid
+
+    counterpart.personal_number = "881015"
+    expect(counterpart).to_not be_valid
+  end
 end
