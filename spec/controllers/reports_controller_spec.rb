2825cf0c870e32f560c811f3610c452e437ae240
me222wm <me222wm@student.lnu.se>
2016-07-02 17:27:28 +0200

    Test for letter template.

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -55,4 +55,30 @@ RSpec.describe ReportsController, type: :controller do
       end
     end
   end
+
+  describe "GET letter template" do
+    context "when not signed in" do
+      it "should fail" do
+        get :letter_template, id: client.id
+        expect(response).to_not be_success
+        expect(response).to redirect_to(new_user_session_path)
+        expect(flash[:alert]).to be_present
+      end
+    end
+
+    context "when signed in" do
+      it "should succeed" do
+        sign_in user
+        get :letter_template, id: client.id, format: :docx
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "should not get letter template other firms reports" do
+        sign_in user
+        get :work_report, id: another_client.id
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
